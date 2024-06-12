1fd6c8bf2bdb448d175dc570b1c4a05211f44735
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2024-06-12 13:10:01 +0900

    스트링 테이블을 이용해서 특정 단어 달라지게 하는거 구현

--- Source/VisualNovel/Private/BFL/BFL_VN.cpp
+++ Source/VisualNovel/Private/BFL/BFL_VN.cpp
@@ -1,4 +1,19 @@
 #include "BFL/BFL_VN.h"
+#include "../VisualNovel.h"
+#include "Internationalization/StringTable.h"
+#include "Internationalization/StringTableCore.h"
+
+TObjectPtr<UStringTable> UBFL_VN::mKeywordData;
+
+UBFL_VN::UBFL_VN()
+{
+	GetObjectAsset(mKeywordData, UStringTable, "/Game/VN/ST_Keyword.ST_Keyword");
+}
+
+bool UBFL_VN::GetKeyword(FString InText, FString& OutText)
+{
+	return mKeywordData->GetStringTable()->GetSourceString(InText.ToUpper(), OutText);
+}
 
 FString UBFL_VN::ToTargetText(FText InText, bool AddQuotes)
 {
@@ -11,13 +26,59 @@ FString UBFL_VN::ToTargetText(FText InText, bool AddQuotes)
 	if(AddQuotes)
 	{
 		FString leftStr;
-		FString rightStr;
-		processString.Split(TEXT(">"), &leftStr, &rightStr);
+		processString.Split(TEXT(">"), &leftStr, nullptr);
+		processString.RemoveFromStart(leftStr);
+		processString=processString.RightChop(1);
 		processString= leftStr+ TEXT(">\"") + processString + TEXT("\"");
+	}
+
+	TArray<FString> words;
+	processString.ParseIntoArray(words, TEXT(" "),true);
+	processString.Empty();
+	FString lastTextStyle;
+	for (FString& curStr : words)
+	{
+		int32 index = curStr.Find(TEXT(">"));
+		if(index!=INDEX_NONE)
+		{			
+			lastTextStyle = curStr.Mid(0, index+1);
+		}
+		FString noSymbolStr = RemoveSymbolText(curStr);
+		FString keyword;
+		if(GetKeyword(noSymbolStr, keyword))
+		{
+			FString rSymbolStr;
+			FString lSymbolStr;
+			curStr.Split(noSymbolStr, &rSymbolStr, &lSymbolStr);
+
+			processString += rSymbolStr;
+			processString += keyword;
+			processString += noSymbolStr;
+			processString += lastTextStyle;
+			processString += lSymbolStr;
+		}
+		else
+		{
+			processString += curStr;
+		}
+		processString += TEXT(" ");
 	}
 
 	processString.ReplaceInline(TEXT("<"), TEXT("</><"));
 	processString=processString.RightChop(3);
 	processString= processString + TEXT("</>");
 	return processString;
+}
+
+FString UBFL_VN::RemoveSymbolText(FString InText)
+{
+	FString tempStr = InText;
+	InText.Split(TEXT(">"), nullptr, &tempStr);
+	tempStr.ReplaceInline(TEXT("."), TEXT(""));
+	tempStr.ReplaceInline(TEXT("?"), TEXT(""));
+	tempStr.ReplaceInline(TEXT("!"), TEXT(""));
+	tempStr.ReplaceInline(TEXT(","), TEXT(""));
+	tempStr.ReplaceInline(TEXT("\""), TEXT(""));
+	tempStr.ReplaceInline(TEXT("'"), TEXT(""));
+	return tempStr;
 }
--- Source/VisualNovel/Public/BFL/BFL_VN.h
+++ Source/VisualNovel/Public/BFL/BFL_VN.h
@@ -8,6 +8,17 @@ class VISUALNOVEL_API UBFL_VN : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 public:
+	UBFL_VN();
+protected:
+	static TObjectPtr<UStringTable> mKeywordData;
+
+public:
 	UFUNCTION(BlueprintPure, Category = "VisualNovel")
-	static FString ToTargetText(FText InText,bool AddQuotes);
+	static bool GetKeyword(FString InText,FString& OutText);
+
+	UFUNCTION(BlueprintPure, Category = "VisualNovel")
+	static FString ToTargetText(FText InText,bool AddQuotes=false);
+
+	UFUNCTION(BlueprintPure, Category = "VisualNovel")
+	static FString RemoveSymbolText(FString InText);
 };
